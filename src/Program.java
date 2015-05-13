e66b97d52d919145ce3d7490663ff3aaaaa79506
Johndale Alfred julian <j.julian@klab.com>
2015-05-13 17:27:01 +0800

    Implemented gallons to liters conversion

--- src/Program.java
+++ src/Program.java
@@ -4,6 +4,14 @@ public class Program {
 
 	public static void main(String args[]){
 		
-		//code goes here
+		double gallons;
+		double liters;
+		
+		Scanner scnr = new Scanner(System.in);
+		System.out.println("Enter gallons: ");
+		gallons = scnr.nextInt();
+		
+		liters = gallons * 3.7854f; //convert to liters
+		System.out.println(gallons + " Gallons converted to " + liters + " liters");
 	}
 }
