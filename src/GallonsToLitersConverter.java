7924c53f4c68cf5fd13003350f92fc08d2222431
Johndale Alfred julian <j.julian@klab.com>
2015-05-14 08:33:00 +0800

    Implemented improved version of Gallons to Liters converter

--- src/GallonsToLitersConverter.java
+++ src/GallonsToLitersConverter.java
@@ -1,5 +1,6 @@
 import java.util.*;
 
+/* Improved version */
 public class GallonsToLitersConverter {
 
 	public static void main(String args[]){
@@ -7,11 +8,27 @@ public class GallonsToLitersConverter {
 		double gallons;
 		double liters;
 		
+		int counter = 0;
+		
+		for(gallons = 1; gallons <= 100; gallons++){
+			liters = gallons * 3.7854f; //convert to liters
+			System.out.println(gallons + " gallons is " + liters + " liters.");
+			
+			counter++;
+			//for every tenth line, print a blank line
+			if(counter == 10){
+				System.out.println();
+				counter = 0; //reset counter;
+			}
+		}
+		
+		/*
 		Scanner scnr = new Scanner(System.in);
 		System.out.println("Enter gallons: ");
 		gallons = scnr.nextInt();
 		
 		liters = gallons * 3.7854f; //convert to liters
 		System.out.println(gallons + " Gallons converted to " + liters + " liters");
+		*/
 	}
 }
