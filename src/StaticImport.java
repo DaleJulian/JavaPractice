40536cfe6290f7b17b88516296c59456aae38d6b
Johndale Alfred julian <j.julian@klab.com>
2015-05-20 14:52:54 +0800

    Static import Math funcs

---
+++ src/StaticImport.java
@@ -0,0 +1,22 @@
+
+
+import static java.lang.Math.sqrt;
+import static java.lang.Math.pow;
+
+public class StaticImport {
+	
+	public static void main(String args[]){
+		
+		double a, b, c, x;
+		
+		a = 4;
+		b = 1; 
+		c = -3;
+		
+		x = (-b + sqrt(pow(b, 2) - 4 * a * c)) / (2 * a);
+		System.out.println("First solution: " + x);
+		
+		x = (-b + sqrt(pow(b, 2) - 4 * a * c)) / (2 * a);
+		System.out.println("Second solution: " + x); 
+	}
+}
