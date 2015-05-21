28a7c1b17a41473e6756e33c0e1c07ec55c42f24
Johndale Alfred julian <j.julian@klab.com>
2015-05-21 10:44:42 +0800

    Implemented drawString

--- src/SimpleApplet.java
+++ src/SimpleApplet.java
@@ -1,9 +1,39 @@
 import java.awt.*;
 import java.applet.*;
 
+
+/*BASIC APPLET SKELETON*/
+
 public class SimpleApplet extends Applet {
 
+	//called first
+	public void init(){
+		//initialization
+	}
+	
+	//called second and everytime the applet
+	//restarts
+	public void start(){
+		//start or resume execution
+	}
+	
+	//called when the applet is stopped
+	public void stop(){
+		//suspends execution
+	}
+	
+	//called when the applet is terminated
+	//this is the last method executed
+	public void destroy(){
+		//perform shutdown activities
+	}
+	
+	//called when an AWT-based applet's window
+	//must be restored
 	public void paint(Graphics g){
-		g.drawString("Java is ozzm", 20, 20);
+		//redisplay contents of window
+		g.drawString("Java is ozzm!!", 20, 20);
 	}
 }
+
+
